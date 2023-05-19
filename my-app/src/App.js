442a42475b67d180407066ff7842efca6a74e8f9
Den <den_asmos@mail.ru>
2023-05-19 11:35:40 +0300

    all buttons in one array

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,7 @@
 import styles from './App.module.css';
 import { useState } from 'react';
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-const operations = ['+', '-'];
+const buttons = ['C', '=', '+', '-', 1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
 export const App = () => {
 	const [input, setInput] = useState('');
@@ -27,19 +26,49 @@ export const App = () => {
 	};
 
 	const renderButtons = (array) => {
-		return array.map((item) => (
-			<button
-				key={item}
-				onClick={(event) => {
-					if (displayColor) {
-						toggleColor();
-					}
-					handleInput(event);
-				}}
-			>
-				{item}
-			</button>
-		));
+		return array.map((item, index) => {
+			if (index === 0) {
+				return (
+					<button
+						className={styles.deleteAndResult}
+						onClick={() => {
+							if (displayColor) {
+								toggleColor();
+							}
+							setInput('');
+						}}
+					>
+						C
+					</button>
+				);
+			} else if (index === 1) {
+				return (
+					<button
+						className={styles.deleteAndResult}
+						onClick={() => {
+							toggleColor();
+							calculateResult();
+						}}
+					>
+						=
+					</button>
+				);
+			} else {
+				return (
+					<button
+						key={item}
+						onClick={(event) => {
+							if (displayColor) {
+								toggleColor();
+							}
+							handleInput(event);
+						}}
+					>
+						{item}
+					</button>
+				);
+			}
+		});
 	};
 
 	return (
@@ -47,29 +76,7 @@ export const App = () => {
 			<p className={displayColor ? styles.displayActive : styles.display}>
 				{input ? input : 0}
 			</p>
-			<div className={styles.functions}>
-				<div className={styles.plusAndMinus}>{renderButtons(operations)}</div>
-				<button
-					onClick={() => {
-						if (displayColor) {
-							toggleColor();
-						}
-						setInput('');
-					}}
-				>
-					C
-				</button>
-				<button
-					onClick={() => {
-						toggleColor();
-						calculateResult();
-					}}
-				>
-					=
-				</button>
-			</div>
-
-			<div className={styles.numbers}>{renderButtons(numbers)}</div>
+			<div className={styles.buttons}>{renderButtons(buttons)}</div>
 		</div>
 	);
 };
